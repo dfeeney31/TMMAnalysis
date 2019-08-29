0e095ea851b1ecdc93931966c77cc260837f4e95
Dan Feeney <daniel.feeney@colorado.edu>
2019-08-29 15:33:04 -0600

    Adding automation across subjects with Regex

--- golfCollate.m
+++ golfCollate.m
@@ -1,15 +1,22 @@
 %% Collate the point estimates from the golf study
 clear
-cd 'C:\Users\Daniel.Feeney\Dropbox (Boa)\Golf\Golf Pilot Aug 2019\DH'
+cd 'C:\Users\Daniel.Feeney\Dropbox (Boa)\Golf\Golf Pilot Aug 2019\AllSubjectPoints'
 files = dir('*.txt');
 outputDat = zeros(11,length(files));
 addpath('C:\Users\Daniel.Feeney\Dropbox (Boa)\Golf\Golf Pilot Aug 2019') 
 counter = 1;
 for file = files'
-    outputDat(:,counter) = importPoints(file.name);
+    outputDat(1:11,counter) = importPoints(file.name);
+    tmp_metadata = strsplit(convertCharsToStrings(file.name),' ');
+    subjectNo(counter) = tmp_metadata(1,1);
+    conditionNo(counter) = tmp_metadata(1,2);
+    trial(counter) = tmp_metadata(1,3);
     counter = counter +1;
 end
 
 outputDat = outputDat';
+subjectNo = subjectNo'; conditionNo = conditionNo'; trial = trial';
+
 finalDat = array2table(outputDat(:,1:10), 'VariableNames', {'LAnklePower','RAnkklePower',...
     'LHipPower','RHipPower','RearGRF','RHipAbduction', 'LHipAbduction','LAnkleEversion','RAnkleEversion','FrontGRF'});
+finalDat.subID = [subjectNo]; finalDat.condition = [conditionNo]; finalDat.trial = [trial];
